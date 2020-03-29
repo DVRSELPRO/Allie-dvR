428626490cefef78d7a19c19df4db149373965b0
DVRSELPRO <59717168+DVRSELPRO@users.noreply.github.com>
2020-03-28 18:44:13 -0600

    Update varsGlobal.php

--- utilidades/varsGlobal.php
+++ utilidades/varsGlobal.php
@@ -1,9 +1,9 @@
 <?php
-define('HOST_URL', 'bsdservicios.net');
+define('HOST_URL', '');
 define('PROJECT_NAME', 'directoresygerentesonline');
 define("CONFIG_MAIL_HOST", "smtp.dotster.com");
-define("CONFIG_MAIL_USERNAME", "reclutamiento@directoresygerentesonline.com");
-define("CONFIG_MAIL_PASSWORD", "1m3j0Rqo1");
+define("CONFIG_MAIL_USERNAME", "");
+define("CONFIG_MAIL_PASSWORD", "");
 define("CONFIG_MAIL_SMTPSECURE", "SSL");
 define("CONFIG_MAIL_PORT", "587");
 define("CONFIG_MAIL_SMTPDEBUG", 0);
